a35faea0f6b1743aab5d0c3dba39c34b3e3a1f94
eyob42 <eyobadigeh07@gmail.com>
2026-03-11 18:10:25 +0300

    feat: implement authentication routes and JWT token generation

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ import connectDB from './config/db.js'
 // import errorHandler from './middleware/errorHandler.js'
 import { errorHandler, notFound } from './middleware/errorHandler.js';
 
+import authRoutes from './routes/authRoutes.js'
+
 //ES6 module__dirname alternative
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -39,6 +41,8 @@ app.use(express.urlencoded({extended:true}));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 //Routes
+app.use('/api/auth', authRoutes)
+
 
 app.use(errorHandler);
 
